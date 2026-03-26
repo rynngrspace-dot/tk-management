1f55ccc19d8321778fede45fa7a1d7659b5faf0b
Admin <admin@tk.com>
2026-03-26 02:42:12 -0700

    tesasidjaks

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -18,26 +18,40 @@
 
 
 // @ts-ignore
-import { PrismaClient } from "@/generated/prisma/client"
-import { PrismaPg } from "@prisma/adapter-pg"
+// import { PrismaClient } from "@/generated/prisma/client"
+// import { PrismaPg } from "@prisma/adapter-pg"
+
+// const globalForPrisma = globalThis as any
+
+// function createPrismaClient() {
+//   const isProduction = process.env.NODE_ENV === "production"
+
+//   const connectionString = isProduction
+//     ? process.env.DATABASE_URL // 🔥 pakai pooler di Vercel
+//     : process.env.DIRECT_URL   // 🔥 pakai direct di local
+
+//   const adapter = new PrismaPg({
+//     connectionString,
+//   })
+
+//   return new PrismaClient({ adapter })
+// }
+
+// const prisma = globalForPrisma.prisma ?? createPrismaClient()
+
+// if (process.env.NODE_ENV !== "production") {
+//   globalForPrisma.prisma = prisma
+// }
+
+// export default prisma
+
+import { PrismaClient } from "@prisma/client"
 
 const globalForPrisma = globalThis as any
 
-function createPrismaClient() {
-  const isProduction = process.env.NODE_ENV === "production"
-
-  const connectionString = isProduction
-    ? process.env.DATABASE_URL // 🔥 pakai pooler di Vercel
-    : process.env.DIRECT_URL   // 🔥 pakai direct di local
-
-  const adapter = new PrismaPg({
-    connectionString,
-  })
-
-  return new PrismaClient({ adapter })
-}
-
-const prisma = globalForPrisma.prisma ?? createPrismaClient()
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma
