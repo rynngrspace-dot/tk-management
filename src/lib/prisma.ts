d0a778d8a85da47e01ee4cae806a836661382e61
Admin <admin@tk.com>
2026-03-26 01:46:36 -0700

    tesasidjaks

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,46 @@
-// @ts-ignore - Next.js resolves .ts imports at build time
+// // @ts-ignore - Next.js resolves .ts imports at build time
+// import { PrismaClient } from "@/generated/prisma/client"
+// import { PrismaPg } from "@prisma/adapter-pg"
+
+// const globalForPrisma = globalThis as any
+
+// function createPrismaClient() {
+//   const connectionString = process.env.DIRECT_URL || process.env.DATABASE_URL
+//   const adapter = new PrismaPg({ connectionString })
+//   return new PrismaClient({ adapter })
+// }
+
+// const prisma = globalForPrisma.prisma ?? createPrismaClient()
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+
+// export default prisma
+
+
+// @ts-ignore
 import { PrismaClient } from "@/generated/prisma/client"
 import { PrismaPg } from "@prisma/adapter-pg"
 
 const globalForPrisma = globalThis as any
 
 function createPrismaClient() {
-  const connectionString = process.env.DIRECT_URL || process.env.DATABASE_URL
-  const adapter = new PrismaPg({ connectionString })
+  const isProduction = process.env.NODE_ENV === "production"
+
+  const connectionString = isProduction
+    ? process.env.DATABASE_URL // 🔥 pakai pooler di Vercel
+    : process.env.DIRECT_URL   // 🔥 pakai direct di local
+
+  const adapter = new PrismaPg({
+    connectionString,
+  })
+
   return new PrismaClient({ adapter })
 }
 
 const prisma = globalForPrisma.prisma ?? createPrismaClient()
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma
+}
 
 export default prisma
